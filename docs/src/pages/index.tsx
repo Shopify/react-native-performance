119b1244e53de8997d785dab1f912befaa992d8c
Elvira Burchik <elviraburchik@gmail.com>
2022-10-04 13:10:21 +0200

    docs: update index screen page (#99)

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -3,30 +3,55 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import GitHubButton from 'react-github-btn';
+import classnames from 'classnames';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+const Button = ({children, href}) => {
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <Link className="button button--outline button--primary button--lg margin-horiz--sm" to={href}>
+      {children}
+    </Link>
+  );
+};
+
+const HomeSplash = () => {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+
+  return (
+    <div className={classnames('hero hero-dark', styles.heroBanner)}>
       <div className="container">
+        <img
+          className={classnames(styles.heroBannerLogo, 'margin-vert--md')}
+          src={useBaseUrl('img/icon.png')}
+          alt="React Native Performance logo"
+        />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/fundamentals/getting-started">
-            Documentation
-          </Link>
+        <div className={classnames(styles.heroButtons, 'name', 'margin-vert--md')}>
+          <Button href={useBaseUrl('docs/fundamentals/getting-started')}>Get Started</Button>
         </div>
+        <GitHubButton
+          href="https://github.com/Shopify/react-native-performance"
+          data-icon="octicon-star"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star Shopify/react-native-performance on GitHub"
+        >
+          Star
+        </GitHubButton>
       </div>
-    </header>
+    </div>
   );
-}
+};
 
 export default function Home() {
   return (
     <Layout title="React Native Performance" description="Performance monitoring for React Native apps">
-      <HomepageHeader />
+      <HomeSplash />
       <main>
         <div
           className="container"
